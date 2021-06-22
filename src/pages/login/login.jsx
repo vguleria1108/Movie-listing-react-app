7e1a38dc5ee1c4e51ea4eb0235d614bf878213c9
Vikrant <vikrant@chalkstreet.com>
2021-06-22 23:18:25 +0530

    Login basic ui

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "./login.css";
+
+export default function Login(props) {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <div className="loginWrapper">
+        <div>
+          <div className="formHolder">
+            <h1>Log In</h1>
+            <form>
+              <input type="email" id="email" name="email" placeholder="Email" />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <input type="submit" value="Log In" />
+              Don't have an account? <a>Sign Up</a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
