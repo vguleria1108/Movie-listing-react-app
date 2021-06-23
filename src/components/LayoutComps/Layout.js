4a0e0732963be1ac799522ecd03bad5abb5f929b
Vikrant <vikrant@chalkstreet.com>
2021-06-23 18:02:15 +0530

    Movie card component, listing with pagination

--- src/components/LayoutComps/Layout.js
+++ src/components/LayoutComps/Layout.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
 import Header from "./Header";
-
+import { Layout as antLayout, Menu } from "antd";
+import {
+    GithubOutlined,
+    LinkedinOutlined,
+    FileSearchOutlined,
+} from "@ant-design/icons";
+const { Sider, Content } = antLayout;
 
 export class Layout extends Component {
     constructor() {
@@ -19,25 +25,45 @@ export class Layout extends Component {
                 <div className="layoutWrapper">
                     <div >
                         <Header />
+
                     </div>
                     <div className="layoutBody" >
-                        {this.props.children}
+                        <div className="sliderWrapper">
+                            <Sider
+                                breakpoint="lg"
+                                collapsedWidth="0"
+                                style={{
+                                    height: "100vh",
+                                    position: "sticky",
+                                    top: 0,
+                                    left: 0
+                                }}
+                                className="sliderLayout"
+                            >
+                                <div className="logo">
+                                    <FileSearchOutlined />
+                                    <span>Movie Search</span>
+                                </div>
+                                <Menu
+                                    className="menuWrapper"
+                                    theme="dark"
+                                    mode="inline"
+                                    selectable={false}
+                                >
+                                    <Menu.Item key="1" icon={<GithubOutlined />}>
+                                        Github
+                                    </Menu.Item>
+                                    <Menu.Item key="2" icon={<LinkedinOutlined />}>
+                                        LinkedIn
+                                    </Menu.Item>
+                                </Menu>
+                            </Sider>
+                        </div>
+                        <Content>{this.props.children}</Content>
+
                     </div>
                 </div>
-                <style jsx>{
-                    `
-                    .layoutWrapper{
-                        height: 100%;
-                        display: flex;
-                        flex-direction: column;
-                    }
-                    .layoutBody {
-                        flex: 1;
-                     }
-    
-                    `
-                }
-                </style>
+
             </>
         );
     }
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,47 +1,73 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/LayoutComps/Layout";
-import { Layout as antLayout, Menu } from "antd";
-import {
-  GithubOutlined,
-  LinkedinOutlined,
-  FileSearchOutlined,
-} from "@ant-design/icons";
+
 import "./dashboard.css";
-const { Sider } = antLayout;
+import MovieCard from "../../components/cardComp/movieCard";
+import { showNotification } from "../../utils/common.util";
+import axios from "axios";
+import { Pagination } from "antd";
 export default function Dashboard(props) {
-  useEffect(() => {}, []);
+  const [movieResData, setMovieResDate] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [movieData, setMovieDate] = useState([]);
+
+  useEffect(() => {
+    //was not able to find api that only return fixed amount of results at a time
+    let apiUrl = "https://imdb-api.com/en/API/Top250Movies/k_9mb0z872";
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        if (res.data) {
+          let movieList = res.data.items;
+          setMovieResDate(movieList);
+        } else showNotification("error", "Unable to fetch movies");
+      })
+      .catch((e) => {
+        console.log(e);
+        showNotification("error", "Unable to fetch movies");
+      });
+  }, []);
+
+  useEffect(() => {
+    //limiting data to 20 at a time to demonstrate pagination
+    let movieData = movieResData.slice(
+      currentPage * 20,
+      (currentPage + 1) * 20
+    );
+    setMovieDate(movieData);
+  }, [movieResData, currentPage]);
+
+  const scrollToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
   return (
     <>
       <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-          className="sliderLayout"
-        >
-          <div className="logo">
-            <FileSearchOutlined />
-            <span>Movie Search</span>
+        <div className="movieListWrapper">
+          <div className="movieListing">
+            {movieData.length > 0
+              ? movieData.map((movie) => (
+                  <MovieCard
+                    image={movie?.image}
+                    title={movie?.title}
+                    id={movie?.id}
+                  />
+                ))
+              : ""}
           </div>
-          <Menu
-            className="menuWrapper"
-            theme="dark"
-            mode="inline"
-            selectable={false}
-          >
-            <Menu.Item key="1" icon={<GithubOutlined />}>
-              Github
-            </Menu.Item>
-            <Menu.Item key="2" icon={<LinkedinOutlined />}>
-              LinkedIn
-            </Menu.Item>
-          </Menu>
-        </Sider>
+          <Pagination
+            defaultCurrent={currentPage + 1}
+            total={movieResData.length}
+            onChange={(e) => {
+              setCurrentPage(e - 1);
+              scrollToTop();
+            }}
+            showSizeChanger={false}
+            pageSize={20}
+          />
+        </div>
       </Layout>
     </>
   );
